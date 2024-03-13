d632cfc251b37d081e185e11b1704da4df6ee9f5
impelfin <daeunrhadl@naver.com>
2024-03-13 17:13:47 +0900

    java_install

---
+++ c/endian.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x=1;
+    if(*(char *)&x==1)
+    {
+        printf("Result : %d\n", *(char *)&x);
+        printf("This system is Little-Endian\n");
+    }else{
+        printf("Result : %d\n", *(char *)&x);
+        printf("This system is Big-Endian\n");
+    }
+}
