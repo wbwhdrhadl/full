9a53ca20f293ebcdafad23f8645efdeaac03eb18
impelfin <daeunrhadl@naver.com>
2024-03-25 17:43:37 +0900

    error_fix

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -3,7 +3,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return ("Hello:"World")
+    return {"Hello":"World"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=3000)
