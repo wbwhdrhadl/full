b2536ef1edc45b857d737fc0ae80d1000b813f1f
impelfin <daeunrhadl@naver.com>
2024-03-28 17:41:47 +0900

    shard

--- mongodb/shard/stop_shard.sh
+++ mongodb/shard/stop_shard.sh
@@ -8,7 +8,7 @@ shard2=$(ps -ef | grep 'mongoShard2')
 second1=$(echo ${config} | cut -d " " -f2)
 second2=$(echo ${router} | cut -d " " -f2)
 second3=$(echo ${shard1} | cut -d " " -f2)
-second3=$(echo ${shard2} | cut -d " " -f2)
+second4=$(echo ${shard2} | cut -d " " -f2)
 
 for var in $second1 $second2 $second3 $second4
 do
