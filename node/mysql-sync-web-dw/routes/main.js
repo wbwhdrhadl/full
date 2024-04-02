a905a7db9f25dadeef2c5b1ec31675d98c469528
impelfin <daeunrhadl@naver.com>
2024-04-02 09:47:23 +0900

    data_chart

--- node/mysql-sync-web-dw/routes/main.js
+++ node/mysql-sync-web-dw/routes/main.js
@@ -1,40 +1,70 @@
-const express = require('express') 
+const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('sync-mysql')
-const env = require('dotenv').config({path:"../../.env"});
+const env = require('dotenv').config({ path: "../../.env" });
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 var connection = new mysql({
-    host: process.env.host,
-    user: process.env.user,
-    port: process.env.port,
-    password: process.env.password,
-    database: process.env.database
+    host:process.env.host,
+    user:process.env.user,
+    port:process.env.port,
+    password:process.env.password,
+    database:process.env.database
 });
 
-app.get('/Hello',(req, res) => {
-    res.send("Hello World");
+app.get('/Hello', (req, res) => {
+    res.send("Hello World")
 })
 
 // select all rows from st_info table
 app.get('/select', (req, res) => {
     const result = connection.query("select * from st_info")
     console.log(result);
-    // res.sendStatus(200);
-    res.send(result);
+    // res.send(result);
+    res.writeHead(200);
+    var template = `
+    <!Doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Result</title>
+        </head>
+        <body>
+            <table border="1" margin:auto; text-align:center;>
+            <tr>
+                <th>ST_ID</th>
+                <th>NAME</th>
+                <th>DEPT</th>
+            </tr>
+            `;
+            for (var i=0; i<result.length;i++) {
+            template += `
+            <tr>
+                <th>${result[i]['ST_ID']}</th>
+                <th>${result[i]['NAME']}</th>
+                <th>${result[i]['DEPT']}</th>
+            </tr>
+            `
+    }
+    template += `
+            </table>
+        </body>
+    </html>
+    `;
+    res.end(template);
 })
 
-// update data into st_info table
+// insert data into st_info table
 app.get('/insert', (req, res) => {
     const { ST_ID, NAME, DEPT } = req.query
     const result = connection.query(
-        "insert into st_info values (?,?,?)", [ST_ID, NAME, DEPT]
+        "insert into st_info values (?, ?, ?)", [ST_ID, NAME, DEPT]
     )
     res.redirect('/select');
 })
@@ -48,13 +78,13 @@ app.get('/update', (req, res) => {
     res.redirect('/select');
 })
 
-// delete data into st_info table
+// delete row from st_info table
 app.get('/delete', (req, res) => {
-    const ST_ID= req.query.ST_ID
+    const ST_ID = req.query.ST_ID
     const result = connection.query(
         "delete from st_info where ST_ID=?", [ST_ID]
     )
     res.redirect('/select');
 })
 
-module.exports = app; 
+module.exports = app;
