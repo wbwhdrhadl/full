24466ed69920fe29041e72f3429f379ae496f53e
wbwhdrhadl <daeunrhadl@naver.com>
2024-04-22 17:33:24 +0900

    oracle, python 연결

---
+++ oracle/myterror_table.sql
@@ -0,0 +1,12 @@
+create table myterror(
+    eventid number,
+    iyear number,
+    iday number,
+    country number,
+    country_txt varchar2(255),
+    region number,
+    region_txt varchar2(255),
+    city varchar2(255),
+    latitude number,
+    longitude number
+)
