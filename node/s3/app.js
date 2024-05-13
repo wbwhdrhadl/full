e1b093829e43391402aeafa39fedec79e978b674
wbwhdrhadl <daeunrhadl@naver.com>
2024-05-13 02:11:00 +0000

    s3_upload,delete,download

--- node/s3/app.js
+++ node/s3/app.js
@@ -1,21 +1,25 @@
-const express = require('express') 
+const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 app.set('port', process.env.PORT || 8000)
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
 app.use(morgan('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 var main = require('./routes/main.js')
-app.use('/',main)
+app.use('/', main)
 
 app.listen(app.get('port'), () => {
-    console.log('8000 port: Server Started~!!')
+    var dir = './uploadedFiles'
+    if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+    console.log('8000 Port: Server Started~!!')
 });
-
