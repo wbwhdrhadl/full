640c4d2627bd8198f99b2075450eb2427a194e99
wbwhdrhadl <daeunrhadl@naver.com>
2024-06-11 07:33:10 +0000

    react-Probs, defaultProps

---
+++ react/basic/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+
+function App() {
+  return (
+    <Wrapper>
+      <Hello name="React!!" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
+  );
+}
+
+export default App;
