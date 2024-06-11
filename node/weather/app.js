d87075fc8f6748b53698a051d7b180f07d74b563
wbwhdrhadl <daeunrhadl@naver.com>
2024-06-11 05:54:37 +0000

    react_practice

---
+++ node/weather/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.set('port', process.env.PORT || 8000);
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// mongoose configuration
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://15.164.207.169:27017/test');
+
+var main = require('./routes/main.js');
+app.use('/', main);
+
+app.listen(app.get('port'), () => {
+  console.log('8000 Port: Server Started~!!');
+});
